3f3e100775669926c2647e06b3390d72fc0f8104
Gustaf Mossakowski <gustaf@koenige.org>
2021-10-01 11:07:18 +0200

    mf_mediadblink_media(): allow $foreign_key to be array of IDs, rename parameter; check against this new array, return matches indexed by foreign key

--- mediadblink/_functions.inc.php
+++ mediadblink/_functions.inc.php
@@ -18,24 +18,27 @@
  * @param string $event
  * @param string $folder
  * @param string $category
- * @param string $foreign_key
+ * @param mixed $ids
  */
-function mf_mediadblink_media($event, $folder, $category, $foreign_key) {
+function mf_mediadblink_media($event, $folder, $category, $ids = []) {
 	global $zz_setting;
 	$zz_setting['brick_cms_input'] = 'json';
 	$url = sprintf($zz_setting['mediadblink_website'], $event, $folder);
+//	@todo
+// 	$url .=  '?meta=*'.$event['identifier'];
 	$media = brick_request_external($url, $zz_setting);
 	unset($media['_']); // metadata
 
+	$foreign_keys = !is_array($ids) ? [$ids] : $ids;
 	$matches = [];
 	foreach ($media as $medium) {
 		foreach ($medium['meta'] as $meta) {
-			if ($meta['foreign_key'] !== $foreign_key) continue;
+			if (!in_array($meta['foreign_key'], $foreign_keys)) continue;
 			if ($meta['category_identifier'] !== $category) continue;
 			if (empty($medium['base_filename'])) {
 				wrap_error(sprintf('Preview images missing in Media Database for object ID %d, identifier %s', $medium['object_id'], $medium['identifier']));
 			}
-			$matches[] = $medium;
+			$matches[$meta['foreign_key']] = $medium;
 		}
 	}
 	return $matches;
