1550e369669d954cf7747d03feb6f0354200d206
Gustaf Mossakowski <gustaf@koenige.org>
2021-10-01 10:16:01 +0200

    move export scripts from schachjugend module to mediadblink module, use english keywords

---
+++ zzbrick_request_get/persons.inc.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * mediadblink module
+ * export persons (only if they participated somewhere) into media database
+ *
+ * Part of »Zugzwang Project«
+ * https://www.zugzwang.org/modules/mediadblink
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2014-2015, 2019, 2021 Gustaf Mossakowski
+ */
+
+
+function mod_mediadblink_get_persons($vars) {
+	$sql = 'SELECT DISTINCT person_id AS `objects[foreign_key]`
+			, identifier AS `objects[identifier]`
+			, "Personen" AS `objects[path]`
+			, "person" AS `objects[category]`
+			, IF(geschlecht = "weiblich", "woman", IF(geschlecht = "männlich", "man", NULL)) AS `objects[subcategory]`
+			, contact AS `objects[title][---]`
+			, identifier AS `objects[title][-id]`
+		FROM personen
+		JOIN teilnahmen USING (person_id)
+		JOIN contacts USING (contact_id)
+		WHERE first_name != "unbekannt" AND last_name != "unbekannt"';
+	$data = wrap_db_fetch($sql, 'objects[foreign_key]');
+	
+	// @todo E-Mail-Adresse auslesen
+	return $data;
+}
